bc388243b9433ea2d6454a2c4371ce5e5282876a
Eric Jionghua WANG <871718152@qq.com>
2018-12-02 18:49:37 -0600

    new epa point data loading method for epa panel

--- dashboard/src/AirQ_Storyboard.R
+++ dashboard/src/AirQ_Storyboard.R
@@ -3,7 +3,12 @@
 # -this function create a storyboard's icon color subtitle fieldname and value
 # given a fieldname proposed by the click on one community area
 library('stringi')
-
+library(fields) #rdist for interpolation
+library(rdist)
+library(ramify)
+library(animation)
+library(akima)
+library(rgl)
 #test text in the storyboard
 testtext <<- "When I wrote the following pages, or rather the bulk of them, I lived alone, in the woods, a mile from any neighbor, in a house which I had built myself, on the shore of Walden Pond, in Concord, Massachusetts, and earned my living by the labor of my hands only. I lived there two years and two months. At present I am a sojourner in civilized life again.
 I should not obtrude my affairs so much on the notice of my readers if very particular inquiries had not been made by my townsmen concerning my mode of life, which some would call impertinent, though they do not appear to me at all impertinent, but, considering the circumstances, very natural and pertinent. Some have asked what I got to eat; if I did not feel lonesome; if I was not afraid; and the like." 
@@ -74,13 +79,8 @@ FindtheStory <-function(regionid,BestStory_n,infTable,ncol,nrow,rankmatrix,CPTC,
   va<-(rankidtable[,order(-rankidtable[3,])])
   if(!CPTC||CPTC.rankidtable==""){
     CPTC.rankidtable <- rankidtable #if does compare
-    print("aaa")
-    print(rankidtable)
-    print("ccc")
-    print(CPTC.rankidtable)
   }else{
     va<-(rankidtable[,order(-CPTC.rankidtable[3,])])
-    print("bbb")
   }
   colname <- c("FieldName","Precentage","Value","Rank")
   result <- data.frame(matrix(ncol = 4, nrow = 0))
@@ -105,4 +105,74 @@ AirQGetIcon <- function(Icontext){
   AirQ_PAL <-function(domain,palette = "Blues", bin = NULL ){
   return(colorQuantile(palette, domain = domain))
 }
+
+
+# Handle Air Pollution Data -----------------------------------------------
+  
+# numCores <- detectCores()
+# cl <- makeCluster(numCores)
+if(F){
+datadir = "dashboard/data/epa"
+setwd(datadir)
+epasource<-list.files(getwd(),pattern = "*.csv")
+datefield <- "Date"
+siteidfield <- "Site ID"
+siteidinresulttable <-"SiteID"
+valuefield <- "Daily Mean PM2.5 Concentration"
+i<- 1 
+
+# s_epr<-as.data.frame(scale(as.data.frame(epr),))
+plot_ly(s_epr, x = s_epr$lat, y = s_epr$lon, z = s_epr$`01/05/2018`, type = "surface")
+epr<-CreateAirPollutantPointDataSet(epasource[2],valuefield = "Daily Mean PM10 Concentration")
+epr<-CreateAirPollutantPointDataSet(epasource[3])}
+
+  
+CreateAirPollutantPointDataSet <-function(path, datefield = "Date",siteidfield = "Site ID",siteidinresulttable = "SiteID", valuefield = "",coordinatesfield = c("SITE_LATITUDE","SITE_LONGITUDE")){
+epadataframe <-fread(path)
+valuefield <- names(epadataframe)[5]
+# epadataframe$date <- as.Date(epadataframe$Date,format = "%d/%m/%Y")
+epa_obst<-unique(epadataframe$date)
+
+colname <- c(siteidinresulttable,unique(epadataframe[[datefield]])) #get uniquedata
+rowname <- unique(epadataframe[[siteidfield]])
+rowname <- rowname[order(rowname)]
+
+result <- data.frame(matrix(ncol = length(colname), nrow = length(rowname)))
+colnames(result) <- colname
+result[[1]] <- rowname
+for(i in 2:length(colname)){
+  onetimeframe <- epadataframe[Date == colname[i],
+                               .(mean(get(valuefield))),
+                               by = .(get(siteidfield))]
+  diffrow <- rowname[!rowname %in% onetimeframe$get]
+  diffrow <- cbind(diffrow, rep(NA,length(diffrow)))
+  onetimeframe <- rbind(as.matrix(onetimeframe),diffrow)
+  onetimeframe <- onetimeframe[order(onetimeframe[,1]),]
+  result[[colname[i]]]<-onetimeframe[,2]
+}
+# coordinatesfield<-c(siteidfield,coordinatesfield)
+coord<-epadataframe[,.(lat = mean(get(coordinatesfield[1])),lon = mean(get(coordinatesfield[2])))
+                    ,by = get(siteidfield)]
+result <- cbind(result,coord[,2:3])
+# result <- cbind(result,epadataframe[,..coordinatesfield])
+# result<-merge(result,epadataframe,by.x = siteidinresulttable, by.y = siteidfield,)
+return(result)
+}
+
+
+
+# plot_ly(x = x.si[[1]], y = y.si[[1]], z = ~z.si[[1]]) %>% add_surface()
+# plot_ly(x = x.si, y = y.si, z = unlist(z.si) ,frame = 1:10) %>% add_contour()
+# persp3d(x.si, y.si, z.si, col = "gray")
+
+# parLapply(cl, inputs, processInput)
+
+# stopCluster(cl)
+
+
+
+# kd <- with(MASS::geyser, MASS::kde2d(duration, waiting, n = 50))
+# p <- plot_ly(x = kd$x, y = kd$y, z = kd$z) %>% add_surface()
+
+
 
